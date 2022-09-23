7dd5cecca0d82de6471f79b0e26126a8b7bb6612
yessilyurt <y.yesilyurt14@gmail.com>
2022-09-23 18:49:03 +0100

    style update

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,23 +1,15 @@
 import React from "react";
 
 function Input({ type, placeholder, value, onChange, style }) {
+  console.log(style);
   return (
     <>
-      <label
-        style={{
-          display: "block",
-          marginBottom: "10px",
-          fontSize: "14px",
-          color: "#333",
-        }}
-      >
-        {placeholder}
-      </label>
       <input
         type={type}
         placeholder={placeholder}
         value={value}
         onChange={onChange}
+        style={style}
       />
     </>
   );
