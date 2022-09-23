d50457faf8a1d897154ff3aa87fa1a8e153e84ed
yessilyurt <y.yesilyurt14@gmail.com>
2022-09-23 06:28:44 +0100

    title and icons added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,23 @@
-import React from "react";
-
+import { BsDownload } from "react-icons/bs";
+import { BiLogIn } from "react-icons/bi";
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="flex justify-between items-center m-4 p-4">
+      <div className="ml-8 text-3xl font-bold p-2 text-black">
+        Expense Tracker
+      </div>
+      <div>
+        <button className="bg-transparent hover:text-blue-500 hover:bg-gray-200 text-black font-bold py-2 px-4 rounded">
+          <BsDownload className="inline-block mr-2" />
+          Download
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">
+          <BiLogIn className="inline-block mr-2" />
+          Login
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
